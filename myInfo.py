0bde759154f69259e8858cb8aa4dca7cbb9e866e
brandongovea <157034205+brandongovea@users.noreply.github.com>
2024-01-20 16:39:15 -0500

    my information v2

--- myInfo.py
+++ myInfo.py
@@ -4,3 +4,4 @@
 print("My name is: Brandon Govea")
 print("My address is: 123 Central Ave, Bismark, North Dakota, 12345")
 print("My phone number is: 848-228-9319")
+print("My major is Mathematics with a concentration in Statistics")
