5ad31d75e78a1fc04731f28592edf85f1008d0f3
brandongovea <157034205+brandongovea@users.noreply.github.com>
2024-03-25 15:59:43 -0400

    Add files via upload

--- AverageFromInput.py
+++ AverageFromInput.py
@@ -3,18 +3,30 @@
 #AverageFromInput
 
 def main():
-    number_file = open('numbers.txt', 'r')
-    total = 0
-    count = 0
-    for number in number_file:
-        total += float(number)
-        count += 1
+    try:
+        number_file = open('numbers.txt', 'r')
+        total = 0
+        count = 0
+        for number in number_file:
+            try:
+            
+                total += float(number)
+                count += 1
+                print(f'I read in {count} number(s) Current number is: {number.strip()} Total is: {total:.2f}')
+            except ValueError:
+                print(f'Bad data: {number.strip()} skipping')
 
-    average = total / count
+        if count == 0:
+            print("No numbers found in the file.")
+        else:
+            average = total / count
 
-    print(f'I read in 1 number(s) Current number is: 22.00 Toal is: 22.00')
-    print(f'I read in 2 number(s) Current number is: 14.00 Toal is: 36.00')
-    print(f'I read in 3 number(s) Current number is: -99.00 Toal is: -63.00')
+    except IOError:
+        print("File not found: numbers.txt - existing")
+
+    
+
+
     print("Average: ", average)
         
     pass
